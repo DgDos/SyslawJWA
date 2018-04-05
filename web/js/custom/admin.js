b396ab54e6dd02701b834a2b03ecb2fa71a7f124
golden <golden@golden1027.academia.loc>
2018-04-05 15:29:23 -0500

    Progreso de vista

--- web/js/custom/admin.js
+++ web/js/custom/admin.js
@@ -4,36 +4,11 @@
 
 $(document).ready(function () {
 
-    // cuadrar altura de paneles laterales derechos
-    //setSugeridosHeightAndScroll(true);
-    //setAgendadosHeightAndScroll(true);
-
-
-    //$('#estadisticas_tabla').DataTable();
-
-
     table = $('#estadisticas').DataTable({
-        ajax: {
-            url: "DemandaS",
-            dataSrc: '',
-            data: {
-                'opcion': "allMe"
-            }
-        },
+        ajax: "AdministradorS",
         columns: [
-            {data: 'titulo'},
-            {data: 'fecha_modificacion'},
-            {data: 'fecha_creacion'},
-            {data: null},
-            {data: 'porcentaje'},
-            {data: null},
-        ],
-        columnDefs: [
-            {
-                targets: -1,
-                data: null,
-                defaultContent: '<button type="button" class="btn btn-primary waves-effect">Abrir</button>'
-            }
+            {data: 'id_usuario'},
+            {data: 'veces'}
         ],
         language: {
             url: 'https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json'
@@ -41,52 +16,5 @@ $(document).ready(function () {
         responsive: true,
         order: [[ 1, "desc" ]]
     });
-
-    $('#estadisticas tbody').on('click', 'button', function () {
-        var data = table.row($(this).parents('tr')).data();
-        abrir(data.id_demanda);
-    });
-
-});
-
-
-$('#nueva_demanda_form').on('submit', function () {
-
-
-    console.log("creando nueva demanda");
     
-    $.ajax({
-        type: 'POST',
-        url: "DemandaS",
-        //force to handle it as text
-        data: {
-            'opcion': "create",
-            'titulo': $('#nuevotitulo').val()
-        },
-        dataType: "text",
-        success: function (data) {
-
-            var json = $.parseJSON(data);
-            if (json == true) {
-                // Aqui debe modificar la pagina de alguna forma con jQuery para mostrar el mensaje
-                console.log('demanda agregada');
-            } else {
-                // Aqui debe modificar la pagina de alguna forma con jQuery para mostrar el mensaje
-                console.log('no se encontro el usuario');
-                alert('Error desconocido');
-            }
-        },
-        async: false
-    });
-    
-    $('#nuevaDemanda').modal('hide');
-
-    table.ajax.reload();
-
-    return false;
 });
-
-function abrir(id_demanda) {
-    localStorage.setItem("id_demanda",id_demanda);
-    document.location.href='demanda.jsp';
-}
