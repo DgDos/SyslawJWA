6faffad305d3e64c4fafeaf9d9b44953b3584424
DgDos <fijusohalo@gmail.com>
2018-05-30 16:29:09 -0500

    Pdf con pruebas

---
+++ src/java/Controller/PdfS.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import Dao.DemandaDAO;
+import Model.Demanda;
+import Util.PDF;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author FiJus
+ */
+public class PdfS extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            int idDemanda=Integer.parseInt(request.getParameter("id_demanda"));
+            DemandaDAO d=new DemandaDAO();
+            Demanda demanda=d.getDemandaById(idDemanda);
+            PDF pdf=new PDF();
+            pdf.makePDF(demanda);
+            Gson gson=new Gson();
+            out.print(gson.toJson("http://localhost:8080/SyslawJWA/docs/"+demanda.getDte_id()+".pdf"));
+        } catch (SQLException | URISyntaxException | ClassNotFoundException ex) {
+            Logger.getLogger(PdfS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
